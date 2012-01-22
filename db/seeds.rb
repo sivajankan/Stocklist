c8713a1a7c971e3b9db09b2a7692670ae17438d8
Siva Kandasamy JANKAN <sivajankan@gmail.com>
2012-01-22 18:22:40 -0500

    csv files added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts("Populating NASDAQ Stocks")
+
+require 'csv'
+csv = CSV.parse(File.read("#{Rails.root}/db/NASDAQ_companylist.csv"), :headers => true)
+csv.each do |row| 
+  # puts row["Symbol"] + ", " + row["Name"] + ", " + row["MarketCap"] + ", " + row["IPOyear"] + ", " + row["Sector"] + "-" + row["industry"]
+end
+
+
+open("#{Rails.root}/db/NA.txt") do |roles|
+  roles.read.each_line do |r|
+    name,priority = r.chomp.split("|")
+    if name.is_a?(Array)
+      name = name[0]
+    end
+    role = Role.find_or_create_by_name(name)
+    role.priority = priority
+    role.save
+  end
+end
